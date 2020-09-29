b1868318f6e86d56950e6a7b24fc4944b7cce4be
Artem Bazhanov <wildmanspam@gmail.com>
2020-09-29 10:31:16 +0300

    Get GPS Position Warning! If app dint have permission, posyion will be NULL

--- app/src/main/java/com/example/getgps4/MainActivity.kt
+++ app/src/main/java/com/example/getgps4/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.getgps4
 
+import android.annotation.SuppressLint
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import kotlinx.android.synthetic.main.activity_main.*
+
+private lateinit var fusedLocationClient: FusedLocationProviderClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        obtieneLocalizacion();
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun obtieneLocalizacion(){
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                var latitude = location?.latitude
+                var longitude = location?.longitude
+
+                Txt_Latitude.text = location?.latitude.toString()
+                Txt_Longitude.text = location?.longitude.toString()
+
+            }
     }
 }
