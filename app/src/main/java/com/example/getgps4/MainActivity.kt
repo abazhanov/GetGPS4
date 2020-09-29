7aff4636a0216eb1d7f933d8b5ad9e6dc7d6bd4d
Artem Bazhanov <wildmanspam@gmail.com>
2020-09-29 13:11:29 +0300

    Try to allow permission to GPS

--- app/src/main/java/com/example/getgps4/MainActivity.kt
+++ app/src/main/java/com/example/getgps4/MainActivity.kt
@@ -1,34 +1,53 @@
 package com.example.getgps4
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Intent
 import android.location.Location
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import kotlinx.android.synthetic.main.activity_main.*
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.RuntimePermissions
 
 private lateinit var fusedLocationClient: FusedLocationProviderClient
 
+@RuntimePermissions
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        obtieneLocalizacion();
+
+
+
+       // obtieneLocalizacion();
+        obtieneLocalizacionWthPermissionChek();
     }
 
+
     @SuppressLint("MissingPermission")
+
+    @NeedsPermission(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
     private fun obtieneLocalizacion(){
         fusedLocationClient.lastLocation
             .addOnSuccessListener { location: Location? ->
-                var latitude = location?.latitude
-                var longitude = location?.longitude
+                //var latitude = location?.latitude
+                //var longitude = location?.longitude
 
                 Txt_Latitude.text = location?.latitude.toString()
                 Txt_Longitude.text = location?.longitude.toString()
 
             }
     }
+
 }
